007429329068944d721c8def8a3689fee4519f8f
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-12-11 20:14:01 +0100

    Added a new styles to main component.

--- src/components/main.js
+++ src/components/main.js
@@ -120,37 +120,150 @@ const MainWrapperProjectSection = styled.div`
   grid-template-rows: 1fr;
   max-height: 800px;
   overflow: hidden;
+  cursor: pointer;
 `
+
+const HeaderSectionProject = styled.div`
+  display: block;
+  margin: 0 auto;
+  margin-top: calc(50% - 200px);
+  font-size: 35px;
+  line-height: 53px;
+  text-align: center;
+  height: 200px;
+  width: 300px;
+  color: #fff;
+  padding-left: 5px;
+  position: relative;
+  overflow: hidden;
+  transition: 0.3s ease-in-out;
+`
+
+const HeaderSectionProjectRight = styled.div`
+  display: block;
+  margin: 0 auto;
+  margin-top: calc(50% - 200px);
+  font-size: 25px;
+  line-height: 23px;
+  text-align: center;
+  height: 130px;
+  width: 200px;
+  color: #fff;
+  padding-left: 5px;
+  position: relative;
+  overflow: hidden;
+  transition: 0.3s ease-in-out;
+`
+
+const BackgroudProjectLeft = styled.div`
+  display: grid;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`
+
 const MainWrapperProjectSectionLeft = styled.div`
   display: grid;
   position: relative;
-  height: 100%;
-  transition: 0.5s;
-  & :hover {
+  height: auto;
+  transition: 0.3s ease-in;
+  ${HeaderSectionProject}:after {
+    position: absolute;
+    transition: 0.3s;
+    content: "";
+    width: 0;
+    left: 0%;
+    bottom: 0;
+    height: 4px;
+    background: #c23b37;
+  }
+  :hover {
+    ${BackgroudProjectLeft} {
+      opacity: 1;
+    }
+    ${HeaderSectionProject}:after {
+      width: 100%;
+      left: 0;
+    }
     transform: rotate(2deg) scale(1.1);
   }
 `
+
 const MainWrapperProjectSectionRight = styled.div`
   display: grid;
   grid-template-rows: 0.5fr 0.5fr;
   position: relative;
   overflow: hidden;
 `
+
+const BackgroudProjectTop = styled.div`
+  display: grid;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`
+
 const MainWrapperProjectSectionRightTop = styled.div`
   display: grid;
   height: auto;
-  transition: 0.5s; 
-  overflow: hidden;
-  & :hover {
+  transition: 0.5s;
+  ${HeaderSectionProjectRight}:after {
+    position: absolute;
+    transition: 0.3s;
+    content: "";
+    width: 0;
+    left: 0%;
+    bottom: 0;
+    height: 4px;
+    background: #c23b37;
+  }
+  :hover {
+    ${BackgroudProjectTop} {
+      opacity: 1;
+    }
+    ${HeaderSectionProjectRight}:after {
+      width: 100%;
+      left: 0;
+    }
     transform: rotate(2deg) scale(1.1);
   }
 `
+
+const BackgroudProjectBottom = styled.div`
+  display: grid;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`
+
 const MainWrapperProjectSectionRightBottom = styled.div`
   display: grid;
   height: auto;
-  overflow: hidden;
   transition: 0.5s;
-  & :hover {
+  ${HeaderSectionProjectRight}:after {
+    position: absolute;
+    transition: 0.3s;
+    content: "";
+    width: 0;
+    left: 0%;
+    bottom: 0;
+    height: 4px;
+    background: #c23b37;
+  }
+  :hover {
+    ${BackgroudProjectBottom} {
+      opacity: 1;
+    }
+    ${HeaderSectionProjectRight}:after {
+      width: 100%;
+      left: 0;
+    }
     transform: rotate(2deg) scale(1.1);
   }
 `
@@ -168,7 +281,6 @@ const MainWrapperOferContent = styled.div`
   justify-self: center;
   position: absolute;
   width: 80%;
-
   padding: 50px 0 185px 0;
   font-size: 20px;
   font-weight: 400;
@@ -235,17 +347,11 @@ const MainWrapperPortfolioSectionContentItemHeader = styled.div`
     color: #000;
   }
 `
-const PhotoPortfolio = styled(Image)`
-  background-size: cover;
-  height: 100%;
-  width: 100%;
-  display: block;
-`
 
-const MainWrapperPortfolioSectionContentItem = styled.button`
+const MainWrapperPortfolioSectionContentItem = styled.div`
   width: 310px;
   height: 560px;
-  border: 3px dotted transparent;
+  border: 3px dotted black;
   transition: 0.3s;
   font-size: 35px;
   font-family: "Lovelo Black";
@@ -288,11 +394,16 @@ const PhotoProject = styled(Image)`
 const PhotoProjectRight = styled(Image)`
   max-width: 100%;
   max-height: 100%;
-  display: block;
   object-fit: cover;
 `
 
-const Main = props => (
+const PhotoPortfolio = styled(Image)`
+  width: 20px;
+  height: 100%;
+  display: block;
+`
+
+const Main = () => (
   <MainWrapper>
     <MainWrapperSection>
       <MainWrapperSectionContent>
@@ -310,13 +421,29 @@ const Main = props => (
     <MainWrapperProjectSection>
       <MainWrapperProjectSectionLeft>
         <PhotoProject imgsrc="project1.jpg" />
+        <BackgroudProjectLeft>
+          <HeaderSectionProject>
+            {" "}
+            <h1>usługi</h1>Urząd Gminy 1
+          </HeaderSectionProject>
+        </BackgroudProjectLeft>
       </MainWrapperProjectSectionLeft>
       <MainWrapperProjectSectionRight>
         <MainWrapperProjectSectionRightTop>
           <PhotoProjectRight imgsrc="project2.jpg" />
+          <BackgroudProjectTop>
+            <HeaderSectionProjectRight>
+              <h1>usługi</h1>Urząd Gminy 2
+            </HeaderSectionProjectRight>
+          </BackgroudProjectTop>
         </MainWrapperProjectSectionRightTop>
         <MainWrapperProjectSectionRightBottom>
           <PhotoProjectRight imgsrc="project3.jpg" />
+          <BackgroudProjectBottom>
+            <HeaderSectionProjectRight>
+              <h1>usługi</h1>Urząd Gminy 3
+            </HeaderSectionProjectRight>
+          </BackgroudProjectBottom>
         </MainWrapperProjectSectionRightBottom>
       </MainWrapperProjectSectionRight>
     </MainWrapperProjectSection>
@@ -346,7 +473,10 @@ const Main = props => (
           </MainWrapperPortfolioSectionContentItemFooter>
         </MainWrapperPortfolioSectionContentItem>
         <MainWrapperPortfolioSectionContentItem>
-          <PhotoPortfolio imgsrc="ofert2.jpg" />
+          <div>
+            <PhotoPortfolio imgsrc="ofert2.jpg" />
+          </div>
+
           <MainWrapperPortfolioSectionContentItemHeader>
             Poczta elektroniczna
           </MainWrapperPortfolioSectionContentItemHeader>
@@ -355,13 +485,9 @@ const Main = props => (
           </MainWrapperPortfolioSectionContentItemFooter>
         </MainWrapperPortfolioSectionContentItem>
         <MainWrapperPortfolioSectionContentItem>
-          <PhotoPortfolio imgsrc="ofert3.jpg" />
-          <MainWrapperPortfolioSectionContentItemHeader>
-            Rozwiązania dedykowane
-          </MainWrapperPortfolioSectionContentItemHeader>
-          <MainWrapperPortfolioSectionContentItemFooter>
-            Dowiedz się więcej
-          </MainWrapperPortfolioSectionContentItemFooter>
+          <div>
+            <Image imgsrc="ofert3.jpg" style={{ width: 20 }} />
+          </div>
         </MainWrapperPortfolioSectionContentItem>
       </MainWrapperPortfolioSectionContent>
     </MainWrapperPortfolioSection>
