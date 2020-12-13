571910c452c4a67222ef0781fbe20f10d46f5afe
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-12-13 20:28:58 +0100

    Added favicon.ico

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `First Gatsby Project`,
+    description: `Gatsby project.`,
     author: `NETTOM`,
   },
   plugins: [
@@ -49,7 +49,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
